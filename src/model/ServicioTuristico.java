55860e9abd05895cb856026b06ec8d9658699334
Anthony Navarro <ant.navarrop@duocuc.cl>
2026-07-01 23:16:18 -0400

    1

--- src/model/ServicioTuristico.java
+++ src/model/ServicioTuristico.java
@@ -1,4 +1,18 @@
 package model;
 
 public class ServicioTuristico {
+    private String nombreServicio;
+    private double precioBase;
+
+    public ServicioTuristico(String nombreServicio, double precioBase) {
+        this.nombreServicio = nombreServicio;
+        this.precioBase = precioBase;
+    }
+
+    public String getNombreServicio() { return nombreServicio; }
+    public double getPrecioBase() { return precioBase; }
+
+    public void mostrarInformacion() {
+        System.out.println("Servicio: " + nombreServicio + " | Precio: $" + precioBase);
+    }
 }
