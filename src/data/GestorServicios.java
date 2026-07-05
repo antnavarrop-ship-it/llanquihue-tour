011bd991452a216f2d23307f8d13c1b8d5218ec1
Anthony Navarro <ant.navarrop@duocuc.cl>
2026-07-05 11:18:43 -0400

    git add src/data/GestorServicios.java git commit -m "Feat: Poblar la coleccion con cinco objetos instanciados" -m "Se cargan 5 instancias reales mezclando las tres subclases para cumplir con los requerimientos del caso."

--- src/data/GestorServicios.java
+++ src/data/GestorServicios.java
@@ -1,15 +1,24 @@
 package data;
 
-import model.ServicioTuristico;
+import model.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GestorServicios {
-    // Declaración de la colección polimórfica exigida por el Criterio 2
     private List<ServicioTuristico> listaServicios;
 
     public GestorServicios() {
         this.listaServicios = new ArrayList<>();
+        cargarServiciosIniciales(); // Llamado automático al crearse
+    }
+
+    private void cargarServiciosIniciales() {
+        // Al menos 5 objetos instanciados combinando las subclases
+        listaServicios.add(new RutaGastronomica("Sabores de Frutillar", 45000, 4, "Repostería Alemana"));
+        listaServicios.add(new PaseoLacustre("Navegación Catamarán Todos los Santos", 35000, "Catamarán", true));
+        listaServicios.add(new ExcursionCultural("Iglesias de Chiloé", 60000, "Patrimonio de la Humanidad", true));
+        listaServicios.add(new RutaGastronomica("Curanto en Angelmó", 25000, 2, "Cocina Marina"));
+        listaServicios.add(new PaseoLacustre("Aventura Lago Llanquihue", 55000, "Lancha Rápida", false));
     }
 
     public List<ServicioTuristico> getListaServicios() {
