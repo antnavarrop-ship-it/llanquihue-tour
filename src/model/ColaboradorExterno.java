9bf8eda7a16c27b9a4a66de79f4e687fb88b8b5d
Anthony Navarro <ant.navarrop@duocuc.cl>
2026-07-11 15:23:31 -0400

    agregar clase ColaboradorExterno para completar jerarquia

---
+++ src/model/ColaboradorExterno.java
@@ -0,0 +1,19 @@
+package model;
+
+import javax.swing.JOptionPane;
+
+public class ColaboradorExterno extends RecursoAgencia implements Registrable {
+    private String empresa;
+
+    public ColaboradorExterno(String id, String nombre, String empresa) {
+        super(id, nombre);
+        this.empresa = empresa;
+    }
+
+    @Override
+    public void mostrarResumen() {
+        JOptionPane.showMessageDialog(null,
+                "[COLABORADOR EXTERNO]\nID: " + id + "\nContacto: " + nombre + "\nEmpresa: " + empresa,
+                "Llanquihue Tour - Registro", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
