13469b2e4a9ea2179df0619351c79ee7cc7bb27b
Anthony Navarro <ant.navarrop@duocuc.cl>
2026-07-11 15:22:45 -0400

    implementar clase Vehiculo en paquete model

---
+++ src/model/Vehiculo.java
@@ -0,0 +1,19 @@
+package model;
+
+import javax.swing.JOptionPane;
+
+public class Vehiculo extends RecursoAgencia implements Registrable {
+    private String patente;
+
+    public Vehiculo(String id, String nombre, String patente) {
+        super(id, nombre);
+        this.patente = patente;
+    }
+
+    @Override
+    public void mostrarResumen() {
+        JOptionPane.showMessageDialog(null,
+                "[VEHÍCULO AGENCIA]\nID: " + id + "\nTipo: " + nombre + "\nPatente: " + patente,
+                "Llanquihue Tour - Registro", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
